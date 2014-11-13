0ba1d2ad9f28d9820458b1a33dba14f91d85cde3
alilili <ujnwanghualei@163.com>
2014-11-13 09:07:46 +0800

    第一次提交

---
+++ src/hello/First.scala
@@ -0,0 +1,11 @@
+package hello
+
+
+object First {
+  def main(args: Array[String]) {
+    val one = new Rational(4,2)
+    val two = new Rational(2,3)
+    val sum = one add two
+    printf(sum.toString)
+  }
+}
